c94320e862aececc347a268832c29ee5b25b858f
batzsalmassi <167541223+batzsalmassi@users.noreply.github.com>
2024-07-26 13:03:42 +0300

    Test1 (#5)
    * update file
    
    * Python lesson3

--- main.py
+++ main.py
@@ -3,3 +3,37 @@ num2 = 3
 num3 = True
 num5 = "seanking"
 print(type(num), type(num2), type(num3), type(num5))
+
+
+c = "s"
+d = bin(ord(c))
+print(d)
+
+
+name = "hudi"
+name = "5"
+
+print(name)
+
+x = 16
+y = 3
+
+print (x + y)
+print (x - y)
+print (x * y)
+print (x / y)
+print (x // y)
+print (x % y)
+print (x ** y)
+
+x2 = 16
+y2 = 3
+
+modulu = x-(x2//y2)*y2
+print(modulu)
+
+
+## Swap
+print (x2, y2)
+x2, y2 = y2, x2
+print(x2, y2)
