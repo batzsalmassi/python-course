7db6a960fb5be797605a433494ae4d62d23a84a8
batzsalmassi <sean.salmassi1@gmail.com>
2024-08-20 21:32:11 +0300

    add folders

---
+++ python-homework/directories/check_key.py
@@ -0,0 +1,13 @@
+#check if the k is soon and print the value of the key
+actor= {
+   15:"fkjfjkglk",
+   "sean":15,
+    "yossssi":True,
+    'yosi':19
+}
+
+for k, v in actor.items():
+    if k == "sean":
+        print(f'The value of the key: {k} is: {v}')
+    else:
+        continue
